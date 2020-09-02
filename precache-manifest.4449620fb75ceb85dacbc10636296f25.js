e99a1583ee1f9cc22cbf2c042b6a42b2ca46dbcf
liakutalikhan <46198906+liakutalikhan@users.noreply.github.com>
2020-09-02 19:59:00 +0600

    Updates

---
+++ precache-manifest.4449620fb75ceb85dacbc10636296f25.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a8da8a7fc4651c64fde1b1749b5f882",
+    "url": "/s3wd-app/index.html"
+  },
+  {
+    "revision": "c4a3bf82e844a4d3272f",
+    "url": "/s3wd-app/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "c5fa1c6802fe1641a384",
+    "url": "/s3wd-app/static/css/main.ec7c834d.chunk.css"
+  },
+  {
+    "revision": "c4a3bf82e844a4d3272f",
+    "url": "/s3wd-app/static/js/2.816b274a.chunk.js"
+  },
+  {
+    "revision": "4e5653c2aea1f8f01a4afdf4252ee778",
+    "url": "/s3wd-app/static/js/2.816b274a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5fa1c6802fe1641a384",
+    "url": "/s3wd-app/static/js/main.55adf68e.chunk.js"
+  },
+  {
+    "revision": "b4f0ef55c7f492b08994",
+    "url": "/s3wd-app/static/js/runtime-main.4213b218.js"
+  },
+  {
+    "revision": "f88e1164112f197abfd4735ce9767cb3",
+    "url": "/s3wd-app/static/media/aboutImg2.f88e1164.png"
+  },
+  {
+    "revision": "a7cd7beab80a7d461f44ede8c85d7e3c",
+    "url": "/s3wd-app/static/media/rocket.a7cd7bea.png"
+  },
+  {
+    "revision": "1cdacc67c70fed1d3ee8d007ace6220f",
+    "url": "/s3wd-app/static/media/s3wd-white.1cdacc67.png"
+  },
+  {
+    "revision": "64305e6f72e723c6c02987dcd65275d9",
+    "url": "/s3wd-app/static/media/service1.64305e6f.jpg"
+  },
+  {
+    "revision": "c8cc71c3542b88147d3f5c632944e4c7",
+    "url": "/s3wd-app/static/media/service2.c8cc71c3.jpg"
+  },
+  {
+    "revision": "568b0edb26fb90fddb0f01ca1a6fda50",
+    "url": "/s3wd-app/static/media/service3.568b0edb.jpg"
+  }
+]);
